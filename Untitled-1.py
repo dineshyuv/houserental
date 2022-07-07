f92cb5756a46fc0d5596fb38a5ca282b099d3df7
dineshyuv <dineshyuvi2016@gmail.com>
2022-07-07 21:37:15 +0530

    first comment

---
+++ Untitled-1.py
@@ -0,0 +1,44 @@
+from codecs import getencoder
+from curses import ACS_GEQUAL
+from ssl import _PasswordType
+
+
+df __init__(self):
+   print("default constructor")
+
+def __init(self, UserId: int, UserName: str):
+    self.user_id = UserId
+    self.user_name = UserName
+
+def display_detailes(self):
+    print("User Id      :", self.user_id)
+    print("User Name      :", self.user_name)
+
+def display_show choices(self):
+    display = input("New user:")if
+    display == input("Already registered":) 
+
+class user:
+
+    user_name: str
+    age: int
+    gender: str
+    phone_number: int
+    pass_word: int
+
+def __init__(self, UserName: str, Age: int, Gender: str, PhoneNumber: int, PassWord: int):
+    self.user_name = UserName
+    self.age  = Age
+    self.gender = Gender
+    self.phone_number = PhoneNumber
+    slef.pass_word = _Password
+
+def display_userdetails(self):
+    print("User Name        : ", self.user_name)
+    print("Age              : ", self.age)
+    print("Gender           : ", self.gender)
+    print("Phone Number     : ", self.phone_number)
+    print("Pass Word        : ", self.pass_word)
+    
+
+
